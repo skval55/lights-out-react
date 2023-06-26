56066299c732893c2d0cbdd8bb67acbd7d94d8c1
skval55 <skvaladez@gmail.com>
2023-06-26 14:54:25 -0600

    lights out first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Board from "./Board";
+import "./App.css";
+
+/** Simple app that just shows the LightsOut game. */
+
+function App() {
+  return (
+    // <h1>hello</h1>
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
